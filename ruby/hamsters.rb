11dfd383b4623a41fbe85d8837ac7f553f86b2be
Nickvanderen <ngvanderen@gmail.com>
2017-04-19 20:39:33 -0700

    Adding hamsters.rb starting

---
+++ ruby/hamsters.rb
@@ -0,0 +1,35 @@
+#Name
+
+puts "What is your hamsters name?"
+name = gets.chomp
+
+"#{name} is an excellent name!"
+
+#Squeak
+
+puts "On a scale of 1-10 how squeaky is this hamster?"
+squeak = gets.chomp
+
+"It's about a #{squeak}."to_i
+
+#Color
+
+puts "What color of fur is the hamster?"
+fur = gets.chomp
+
+"#The coloration on this hamster is #{fur}!"
+
+#Adoption
+
+puts "Is this hamster a good canididate for adotoption?"
+adoptable =  gets.chomp
+
+"#{adoptable}"
+
+#Age
+
+
+puts "What is the estimated age of this hamster?"
+age = gets.chomp
+
+puts
